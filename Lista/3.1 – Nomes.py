03c247ca3b857e3e9c93d340d32559f7deb1deab
karolinerlima <karolinerlima@gmail.com>
2022-06-05 19:26:48 -0300

    confirmar master

---
+++ Lista/3.1 – Nomes.py
@@ -0,0 +1,13 @@
+"""
+   3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
+chamada names. Exiba o nome de cada pessoa acessando cada elemento da
+lista, um de cada vez.
+ 
+    
+"""
+
+nome= ['Davi', 'Sophia','Roseli','Wagner']
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[3])
---
+++ Zen/2.11 – Zen de Python.py
@@ -0,0 +1,10 @@
+"""
+    
+2.11 – Zen de Python: Digite import this em uma sessão de terminal de Python
+e dê uma olhada nos princípios adicionais.
+
+"""
+# Now is better than never
+
+import this
+print(this)
