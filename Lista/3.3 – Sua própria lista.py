e61369e1cccd1129d65956bb0343c37c8604ae2e
karolinerlima <karolinerlima@gmail.com>
2022-06-06 20:43:39 -0300

    confirmar master

--- Lista/3.3 – Sua própria lista.py
+++ Lista/3.3 – Sua própria lista.py
@@ -5,3 +5,9 @@ lista para exibir uma série de frases sobre esses itens, como “Gostaria de te
 uma moto Honda”.
 
 """
+from asyncio import transports
+from tracemalloc import take_snapshot
+
+
+transporte = ['carro', 'moto','avião']
+print(f'Gostaria de comprar um {transporte[0]}\n Gostaria de uma {transporte[1]}\n Gostaria de andar de {transporte[2]}')
